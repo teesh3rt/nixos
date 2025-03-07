3949d300ebc42bfd3d61cd01c08082bb2b8bf807
teesh3rt <ilaylevy611@gmail.com>
2025-03-08 00:07:54 +0200

    feat: switch to nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:danth/stylix/release-24.11";
+    stylix.url = "github:danth/stylix";
   };
 
   outputs = { self, nixpkgs, home-manager, stylix, }: let
